45628fad635e974a22ca242ae3a22a8b39eb4582
Saadallah-Kassir <saadallah.kassir@gmail.com>
2016-03-20 13:56:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/saadallah/synapps/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.saadallah.synapps;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private android.support.v7.app.ActionBar bar;
+    private ActionBarDrawerToggle toggle;
+    private DrawerLayout drawer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // drawer stuff!!!!!!!!!!!!!!!!!!!!!!
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        bar = this.getSupportActionBar();
+        bar.setDisplayHomeAsUpEnabled(true);
+        bar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.myBlue)));
+
+        toggle = new ActionBarDrawerToggle(this, drawer,0,0){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        toggle.syncState();
+        drawer.setDrawerListener(toggle);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (!drawer.isDrawerOpen(Gravity.LEFT)){
+            drawer.openDrawer(Gravity.LEFT);
+        }
+        else{
+            drawer.closeDrawer(Gravity.LEFT);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onClickClearData(View view) { //Don't forget to implement this method!
+    }
+
+    public void onGenerateGraphClick(View view) { //Don't forget to implement this method!
+    }
+
+    public void onNetworkDetailsClick(View view) { //Don't forget to implement this method!
+    }
+
+    public void onClickOk(View view) { //Don't forget to implement this method!
+    }
+
+    public void onClickIgnore(View view) { //Don't forget to implement this method!
+    }
+
+    public void onClickYes(View view) { //Don't forget to implement this method!
+    }
+
+    public void onClickNo(View view) { //Don't forget to implement this method!
+
+    }
+}
+
