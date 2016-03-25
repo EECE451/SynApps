320340c7e2a734af08da112307839153eabab9c8
Saadallah-Kassir <saadallah.kassir@gmail.com>
2016-03-25 16:21:05 +0200

    WiFi Direct switch working 100%++

--- app/src/main/java/com/example/saadallah/synapps/Connectivity_State.java
+++ app/src/main/java/com/example/saadallah/synapps/Connectivity_State.java
@@ -16,11 +16,75 @@ public class Connectivity_State extends AppCompatActivity {
 
         Intent receivedIntent = getIntent();
         boolean bt_state_value = receivedIntent.getBooleanExtra("bluetooth_state", false);
+        boolean wifi_state_value = receivedIntent.getBooleanExtra("wifi_state", false);
+        int cell_network_type = receivedIntent.getIntExtra("network_type",0);
+
         TextView bt_value_textview = (TextView) findViewById(R.id.bt_value);
         if (bt_state_value)
             bt_value_textview.setText("Enabled");
         else
             bt_value_textview.setText("Disabled");
+
+        TextView wifi_value_textview = (TextView) findViewById(R.id.wifi_value);
+        if (wifi_state_value)
+            wifi_value_textview.setText("Enabled");
+        else
+            wifi_value_textview.setText("Disabled");
+
+        TextView cell_value_textview = (TextView) findViewById(R.id.cell_value);
+
+        switch (cell_network_type)
+        {
+            case 7:
+                cell_value_textview.setText("1xRTT");
+                break;
+            case 4:
+                cell_value_textview.setText("CDMA");
+                break;
+            case 2:
+                cell_value_textview.setText("EDGE");
+                break;
+            case 14:
+                cell_value_textview.setText("eHRPD");
+                break;
+            case 5:
+                cell_value_textview.setText("EVDO rev. 0");
+                break;
+            case 6:
+                cell_value_textview.setText("EVDO rev. A");
+                break;
+            case 12:
+                cell_value_textview.setText("EVDO rev. B");
+                break;
+            case 1:
+                cell_value_textview.setText("GPRS");
+                break;
+            case 8:
+                cell_value_textview.setText("HSDPA");
+                break;
+            case 10:
+                cell_value_textview.setText("HSPA");
+                break;
+            case 15:
+                cell_value_textview.setText("HSPA+");
+                break;
+            case 9:
+                cell_value_textview.setText("HSUPA");
+                break;
+            case 11:
+                cell_value_textview.setText("iDen");
+                break;
+            case 13:
+                cell_value_textview.setText("LTE");
+                break;
+            case 3:
+                cell_value_textview.setText("UMTS");
+                break;
+            case 0:
+                cell_value_textview.setText("Unknown");
+                break;
+        }
+
 
     }
 
