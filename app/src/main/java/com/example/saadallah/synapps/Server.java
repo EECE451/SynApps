f82179f7d597f47e332543cfe615a415e34aebd6
JadHosn <Jadhosn13@#>
2016-05-02 00:21:22 +0300

    Server Updated and final - last update :: Over and out

--- app/src/main/java/com/example/saadallah/synapps/Server.java
+++ app/src/main/java/com/example/saadallah/synapps/Server.java
@@ -41,7 +41,8 @@ public class Server extends AppCompatActivity {
 
     String insertUrl = "http://192.168.210.1:80/DevicesServer/insertEntry.php";
     String showUrl = "http://192.168.210.1:80/DevicesServer/displayEntry.php";
-    String showspecificUrl = "http://192.168.210.1:80/DevicesServer/displayspecific.php";
+    String showspecificUrl = "http://192.168.210.1:80/DevicesServer/displayspecific2.php";
+    String showSpecificUrl2 = "http://192.168.210.1:80/DevicesServer/displayspecific4.php";
 
 
     // String insertUrl = "http://10.168.46.13:80/DevicesServer/insertEntry.php";
@@ -120,25 +121,100 @@ public class Server extends AppCompatActivity {
         requestQueue.add(jsonObjectRequest);
     }
 
-    public void onbtnclickinsertall(View view) {
+//    public void onbtnclickinsertall(View view) {
+//
+//        Cursor result_NumberofDevices = myDb.getNumberofDevices();
+//        String result_NumberofDevices_String = "";
+//        String MACConnected ="";
+//        String DeviceName = "";
+//        String DateLastDetection = "";
+//        String FrequencyDetection = "";
+//        String CumulativeDetection = "";
+//
+//
+//        if (result_NumberofDevices != null &&result_NumberofDevices.getCount() > 0) {
+//            result_NumberofDevices.moveToFirst();
+//            result_NumberofDevices_String = result_NumberofDevices.getString(0);
+//        }
+//        int result_NumberofDevices_String_int = Integer.parseInt(result_NumberofDevices_String);
+//
+//        for(int i=1;i<=result_NumberofDevices_String_int;i++)
+//        {   Log.d("Loop Flag", String.valueOf(i));
+//
+//            Cursor result_getalldata = myDb.getAllDatabyDid(i);
+//
+//            while (result_getalldata.moveToNext()) {
+//                MACConnected = result_getalldata.getString(0);
+//                DeviceName = result_getalldata.getString(1);
+//                DateLastDetection = result_getalldata.getString(2);
+//                FrequencyDetection = result_getalldata.getString(3);
+//                CumulativeDetection = result_getalldata.getString(4);
+//
+//            }
+//            final String finalDateLastDetection = DateLastDetection;
+//            final String finalDeviceName = DeviceName;
+//            final String finalMACConnected = MACConnected;
+//            final String finalFrequencyDetection = FrequencyDetection;
+//            final String finalCumulativeDetection = CumulativeDetection;
+//
+//            StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//
+//                    System.out.println(response.toString());
+//                }
+//            }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//
+//                }
+//            }) {
+//
+//                @Override
+//                protected Map<String, String> getParams() throws AuthFailureError {
+//                    Map<String,String> parameters  = new HashMap<String, String>();
+//                    parameters.put("MACConnected", finalMACConnected);
+//                    parameters.put("MACMasterDevice",MACMasterDevice);
+//                    parameters.put("DeviceName", finalDeviceName);
+//                    parameters.put("DateLastDetection", finalDateLastDetection);
+//                    parameters.put("FrequencyDetection", finalFrequencyDetection);
+//                    parameters.put("CumulativeDetection", finalCumulativeDetection);
+//                    parameters.put("MasterName","");
+//
+//
+//                    return parameters;
+//                }
+//            };
+//            requestQueue.add(request);
+//        }
+//
+//
+//
+//
+//    }
+
+
+
+    public void onbtnclickinsertall(final View view) {
+
 
         Cursor result_NumberofDevices = myDb.getNumberofDevices();
         String result_NumberofDevices_String = "";
-        String MACConnected ="";
+        String MACConnected = "";
         String DeviceName = "";
         String DateLastDetection = "";
         String FrequencyDetection = "";
         String CumulativeDetection = "";
 
 
-        if (result_NumberofDevices != null &&result_NumberofDevices.getCount() > 0) {
+        if (result_NumberofDevices != null && result_NumberofDevices.getCount() > 0) {
             result_NumberofDevices.moveToFirst();
             result_NumberofDevices_String = result_NumberofDevices.getString(0);
         }
         int result_NumberofDevices_String_int = Integer.parseInt(result_NumberofDevices_String);
 
-        for(int i=1;i<=result_NumberofDevices_String_int;i++)
-        {   Log.d("Loop Flag", String.valueOf(i));
+        for (int i = 1; i <= result_NumberofDevices_String_int; i++) {
+            Log.d("Loop Flag", String.valueOf(i));
 
             Cursor result_getalldata = myDb.getAllDatabyDid(i);
 
@@ -156,11 +232,26 @@ public class Server extends AppCompatActivity {
             final String finalFrequencyDetection = FrequencyDetection;
             final String finalCumulativeDetection = CumulativeDetection;
 
-            StringRequest request = new StringRequest(Request.Method.POST, insertUrl, new Response.Listener<String>() {
+            StringRequest request = new StringRequest(Request.Method.POST, showspecificUrl, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
+                    try {
 
-                    System.out.println(response.toString());
+                        System.out.println(response.toString());
+                        JSONObject jsonObject = new JSONObject(response);
+                        if (Integer.parseInt(jsonObject.getString("exists")) == 1) {
+                            Log.d("batata", "batata");
+                            onbtnspecific(finalMACConnected,finalDeviceName,finalDateLastDetection,finalFrequencyDetection,finalCumulativeDetection);
+                        } else {
+                            Log.d("batata", "Mech batata");
+
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+
                 }
             }, new Response.ErrorListener() {
                 @Override
@@ -168,29 +259,83 @@ public class Server extends AppCompatActivity {
 
                 }
             }) {
-
-                @Override
                 protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String,String> parameters  = new HashMap<String, String>();
+                    Map<String, String> parameters = new HashMap<String, String>();
                     parameters.put("MACConnected", finalMACConnected);
-                    parameters.put("MACMasterDevice",MACMasterDevice);
+                    parameters.put("MACMasterDevice", MACMasterDevice);
                     parameters.put("DeviceName", finalDeviceName);
                     parameters.put("DateLastDetection", finalDateLastDetection);
                     parameters.put("FrequencyDetection", finalFrequencyDetection);
                     parameters.put("CumulativeDetection", finalCumulativeDetection);
-                    parameters.put("MasterName","");
+                    parameters.put("MasterName", "");
 
 
                     return parameters;
                 }
             };
+
             requestQueue.add(request);
         }
+    }
+
+    public void onbtnspecific(final String s1, final String s2, final String s3, final String s4, final String s5) {
+
+
+        StringRequest request = new StringRequest(Request.Method.POST, showSpecificUrl2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+
+                    JSONObject jsonObject = new JSONObject(response);
+
+                    if(jsonObject.names().get(0).equals("exists")) {
+
+                        if(Integer.parseInt(jsonObject.getString("exists")) == 1)
+                        {
+                            Log.d("batata","batata22");
+                        }
+                        else
+                        {
+                            Log.d("batata","Mech batata23");
+
+                        }
+
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parameters  = new HashMap<String, String>();
+                parameters.put("MACConnected",s1);
+                parameters.put("MACMasterDevice",MACMasterDevice);
+                parameters.put("DeviceName",s2);
+                parameters.put("DateLastDetection",s3);
+                parameters.put("FrequencyDetection",s4);
+                parameters.put("CumulativeDetection",s5);
+                parameters.put("MasterName","");
 
 
 
+
+                return parameters;
+            }
+        };
+
+        requestQueue.add(request);
 
     }
+
 
 
 
